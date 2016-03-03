c354661932b363549830deead51bd1ab965b90fc
Andre Almeida <aalmeida@thoughtworks.com>
2016-03-03 14:51:46 -0300

    create Rate Class
    create Rate Class
    create shouldReturnNumberRateWhenSendNumber7 test

---
+++ src/com/twu/model/Rate.java
@@ -0,0 +1,14 @@
+package com.twu.model;
+
+
+public class Rate {
+    private int rate;
+
+    public Rate(int rate){
+        this.rate = rate;
+    }
+
+    public int getRate(){
+        return this.rate;
+    }
+}
