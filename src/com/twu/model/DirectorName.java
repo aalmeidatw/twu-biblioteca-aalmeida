c1cd608fa101207dd081bf9cf195b916efd9a2da
Andre Almeida <aalmeida@thoughtworks.com>
2016-03-03 14:44:30 -0300

    create DirectorName Class
    crate directorName class
    create shouldReturnDirectorName test

---
+++ src/com/twu/model/DirectorName.java
@@ -0,0 +1,14 @@
+package com.twu.model;
+
+
+public class DirectorName {
+    private String directorName;
+
+    public DirectorName(String name){
+        this.directorName = name;
+    }
+
+    public String getDirectorName(){
+        return this.directorName;
+    }
+}
---
+++ test/com/twu/model/DirectorNameTest.java
@@ -0,0 +1,22 @@
+package com.twu.model;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class DirectorNameTest {
+    private DirectorName directorName;
+
+    @Before
+    public void setUp() throws Exception {
+        this.directorName = new DirectorName("Matthew Ferreira");
+    }
+
+    @Test
+    public void shouldReturnDirectorName(){
+        assertThat(directorName.getDirectorName(), is("Matthew Ferreira"));
+    }
+}
