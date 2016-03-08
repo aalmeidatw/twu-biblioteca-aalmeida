d0c6723b1d0467bc8a88d72af6837259fdcc6b47
Andre Almeida <aalmeida@thoughtworks.com>
2016-03-08 15:39:56 -0300

    create getListOfBooksToReturn in Library Class

--- src/com/twu/control/Library.java
+++ src/com/twu/control/Library.java
@@ -2,39 +2,37 @@ package com.twu.control;
 
 
 import com.twu.book.Book;
-import com.twu.book.BookAlgorithm;
 import com.twu.model.AuthorName;
 import com.twu.model.BookName;
 import com.twu.model.PublicationYear;
-import com.twu.scanner.ScannerInputUser;
 
 import java.util.ArrayList;
 
 public class Library {
     private ArrayList<Book> myBooks;
     private ArrayList<Book> availableBooks = new ArrayList<>();
-    private BookAlgorithm bookAlgorithm;
-    private ScannerInputUser scannerInputUser = new ScannerInputUser();
+    private ArrayList<Book> booksToReturn = new ArrayList<>();
     private Book myBook;
 
     public Library(ArrayList<Book> myBooks){
         this.myBooks = myBooks;
     }
 
-    public void returnBook(int value){
-        myBooks.get(value).setBookIsAvailable();
+    public void returnBook(String bookName){
+        myBooks.forEach(book -> {
+                if(book.getBookName().equals(bookName)){
+                    book.setBookIsAvailable();
+                }});
     }
 
 
-    public ArrayList<Book> getAvailableBooks(){
-
+    public ArrayList<Book> getAvailableBooksList(){
         this.myBooks.forEach(book ->{
             if(book.isAvailable()){
                 this.availableBooks.add(book);
             }
         });
      return this.availableBooks;
-
     }
 
     public void markBookAsReserved(int value){
@@ -43,10 +41,8 @@ public class Library {
         this.myBooks.forEach(book -> {
             if(book.getBookName().equals(bookName)){
                 book.setBookIsNotAvailable();
-        }});
+             }});
     }
-
-
 
     public Book getBookForDetail(int value){
         String bookName = getNameAtBook(value);
@@ -54,8 +50,7 @@ public class Library {
        this.availableBooks.forEach(book -> {
                 if(book.getBookName().equals(bookName)){
                     this.myBook =  new Book(new BookName(book.getBookName()),new AuthorName(book.getAuthorName()), new PublicationYear(book.getPublicationYear()), true);
-                }
-        });
+                }});
         return myBook;
     }
 
@@ -63,6 +58,17 @@ public class Library {
     private String getNameAtBook(int value){
         return this.availableBooks.get(value).getBookName();
     }
+
+    public ArrayList<Book> getListOfBooksToReturn(){
+        this.myBooks.forEach(book ->{
+            if(!book.isAvailable()){
+                this.booksToReturn.add(book);
+            }
+        });
+        return this.booksToReturn;
+    }
+
+
 
 
 
--- src/com/twu/libraryApp/LibraryApp.java
+++ src/com/twu/libraryApp/LibraryApp.java
@@ -1,6 +1,5 @@
 package com.twu.libraryApp;
 import com.twu.book.Book;
-import com.twu.book.BookAlgorithm;
 import com.twu.book.BookList;
 import com.twu.control.Library;
 import com.twu.message.PrinterMessage;
@@ -13,26 +12,27 @@ public class LibraryApp {
 
     public static void main(String[] args) {
         ArrayList<Book> availableBooks = new ArrayList<>();
+        ArrayList<Book> booksToReturn = new ArrayList<>();
         PrinterMessage printerMessage = new PrinterMessage();
         ScannerInputUser scannerInputUser = new ScannerInputUser();
         ArrayList<Book> myBooks = new BookList().createBookList();
         Library library = new Library(myBooks);
-        BookAlgorithm bookAlgorithm = new BookAlgorithm(myBooks);
+
 
         printerMessage.welcomeMessage();
         boolean quitSystem = false;
 
 
      do {
-         //printerMessage.selectedOptionMessage();
-         printerMessage.showOtherFunctionsMessage();
 
+         printerMessage.showOtherFunctionsMessage();
          String option = scannerInputUser.getUserInfoScanner();
+
 
          switch (option) {
              case "L":
                  printerMessage.printMessageOnConsole("List of Available Books! : ");
-                 availableBooks = library.getAvailableBooks();
+                 availableBooks = library.getAvailableBooksList();
 
                  availableBooks.forEach(book -> System.out.println( " " + book.getBookName()));
                  break;
@@ -43,11 +43,18 @@ public class LibraryApp {
                  break;
 
              case "R":
-                 //bookAlgorithm.checkoutBook();
+                //bookAlgorithm.checkoutBook();
                  break;
 
              case "C":
-                 //bookAlgorithm.bookReturn();
+                 printerMessage.printMessageOnConsole("List of Books To Return : ");
+                 booksToReturn = library.getListOfBooksToReturn();
+
+                 booksToReturn.forEach(book -> System.out.println( " " + book.getBookName()));
+                 System.out.println("Select book to Return:");
+                 int value = scannerInputUser.getInputIndex();
+                 library.returnBook(booksToReturn.get(value).getBookName());
+                 System.out.print("Tks!");
                  break;
 
              case "Q":
